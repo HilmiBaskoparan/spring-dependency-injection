0380bd35772f1d8f35ee25fa058d11e3d8563886
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-02-17 09:47:14 +0300

    All configuration of services in "GreetingServiceConfig.java". Primary and Profile Configuration

--- src/main/java/hilmi/springframework/springdependencyinjection/config/GreetingServiceConfig.java
+++ src/main/java/hilmi/springframework/springdependencyinjection/config/GreetingServiceConfig.java
@@ -1,13 +1,31 @@
 package hilmi.springframework.springdependencyinjection.config;
 
-import hilmi.springframework.springdependencyinjection.services.ConstructorGreetingService;
-import hilmi.springframework.springdependencyinjection.services.PropertyInjectedGreetingService;
-import hilmi.springframework.springdependencyinjection.services.SetterInjectedGreetingService;
+import hilmi.springframework.springdependencyinjection.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService")
+    I18nSpanishGreetingService i18nSpanishGreetingService() {
+        return new I18nSpanishGreetingService();
+    }
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService i18nService() {
+        return new I18nEnglishGreetingService();
+    }
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
+    }
 
     @Bean
     ConstructorGreetingService constructorGreetingService() {
--- src/main/java/hilmi/springframework/springdependencyinjection/services/PrimaryGreetingService.java
+++ src/main/java/hilmi/springframework/springdependencyinjection/services/PrimaryGreetingService.java
@@ -1,10 +1,5 @@
 package hilmi.springframework.springdependencyinjection.services;
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Service;
-
-@Primary
-@Service
 public class PrimaryGreetingService implements GreetingService {
 
     @Override
