b469227f28b23bff419952013fd21978313bc293
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-02-17 15:50:11 +0300

    Spring XML Configuration Example

--- src/main/java/hilmi/springframework/springdependencyinjection/config/GreetingServiceConfig.java
+++ src/main/java/hilmi/springframework/springdependencyinjection/config/GreetingServiceConfig.java
@@ -5,11 +5,9 @@ import com.springframework.pets.PetServiceFactory;
 import hilmi.springframework.springdependencyinjection.repositories.EnglishGreetingRepository;
 import hilmi.springframework.springdependencyinjection.repositories.EnglishGreetingRepositoryImpl;
 import hilmi.springframework.springdependencyinjection.services.*;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.*;
 
+@ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
 
@@ -53,10 +51,13 @@ public class GreetingServiceConfig {
         return new PrimaryGreetingService();
     }
 
+    /*
+    This bean is in sfgdi-config.xml
     @Bean
     ConstructorGreetingService constructorGreetingService() {
         return new ConstructorGreetingService();
     }
+     */
 
     @Bean
     PropertyInjectedGreetingService propertyInjectedGreetingService() {
