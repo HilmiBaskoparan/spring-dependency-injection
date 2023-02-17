17d7f415fb9de9e7b9be7dde4ee5dc0c2b238efe
Hilmi Baskoparan <h.baskoparan@gmail.com>
2023-02-17 09:30:23 +0300

    Using @Configuration instead of @Service.

--- src/main/java/hilmi/springframework/springdependencyinjection/services/SetterInjectedGreetingService.java
+++ src/main/java/hilmi/springframework/springdependencyinjection/services/SetterInjectedGreetingService.java
@@ -1,7 +1,5 @@
 package hilmi.springframework.springdependencyinjection.services;
 
-import org.springframework.stereotype.Service;
-
 public class SetterInjectedGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
